e1d1233b92b6f68faa71383930649d6dd591332e
Dave <Bla@blub.de>
2013-05-11 14:00:08 +0200

    Hallo Andreas
    Hallo

---
+++ EvolutionAlgo/EvolutionAlgo/Generation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvolutionAlgo
+{
+    class Generation
+    {
+        public Generation(int size)
+        {
+
+
+        }
+    }
+}
