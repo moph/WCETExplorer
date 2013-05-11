ac0bfb56ee61cf4a60ba46689328c8d1c3a3d889
Andreas Engel <aengel87@web.de>
2013-05-11 14:19:38 +0200

    start to create EvolutionAlgo

---
+++ EvolutionAlgo/EvolutionAlgo/EvolutionAlgo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvolutionAlgo
+{
+    class EvolutionAlgo
+    {
+        private uint _startSize;
+        private AlgoSettings _settings;
+        private float[] _analog;
+        private bool[] _digital;
+        private int[] _enmus;
+
+        EvolutionAlgo(Parameter param);
+    }
+}
