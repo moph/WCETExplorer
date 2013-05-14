1defbd26d8993b3b9d2747ecd738ff150f793d3d
Dave <Bla@blub.de>
2013-05-14 10:15:33 +0200

    Test Git

--- EvolutionAlgo/EvolutionAlgo/Parameter.cs
+++ EvolutionAlgo/EvolutionAlgo/Parameter.cs
@@ -10,12 +10,16 @@ namespace EvolutionAlgo
         public float[] analog;
         public bool[] digital;
         public int[] enums;
+        int test12;
         
         public Parameter(float[] analog, bool[] digital,int[] enums) {
             this.analog = analog;
             this.digital = digital;
             this.enums = enums;
             //Dies ist ein GitTest
+
+
+            
         }
         
     }
