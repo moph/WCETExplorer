ad7c2245b118fde41515aeeb3db16b2444ffcdd0
MoonE <maxi_kroeg@web.de>
2013-05-14 14:45:41 +0200

    renamed Repository folder to WCETExplorer
    Added Gui-Project

---
+++ WCETExplorer/Gui/WManualSettings.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Windows.Controls.Ribbon;
+
+namespace Gui
+{
+    /// <summary>
+    /// Interaction logic for RibbonWindow3.xaml
+    /// </summary>
+    public partial class RibbonWindow4 : RibbonWindow
+    {
+        public RibbonWindow4()
+        {
+            InitializeComponent();
+            
+            // Insert code required on object creation below this point.
+        }
+    }
+}
