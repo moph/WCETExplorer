86959ed22a8a37de1a41fc2ff3b2cff08fef95ad
Dave <Bla@blub.de>
2013-05-14 13:46:53 +0200

    Generation WIP

--- EvolutionAlgo/EvolutionAlgo/Algo/Generation.cs
+++ EvolutionAlgo/EvolutionAlgo/Algo/Generation.cs
@@ -38,18 +38,30 @@ namespace EvolutionAlgo
         public void createGenes()
         {
             Parameter genomParameter;
-            Random ran = new Random();
+            Random ran;
             int countAnalog = _blaram.analog.Length;
             int countDigital = _blaram.digital.Length;
             int countEnums = _blaram.enums.Length;
             float[] analogVal = new float[countAnalog];
+            bool[] digitalVal = new bool[countDigital];
+            int[] enumVal = new int[countEnums];
             for (int k = 0; k < this._size; k++)
             {
+                ran = new Random();
                 for (int i = 0; i < countAnalog; i++)
                 {
 
                     analogVal[i] = (float)ran.NextDouble();
                 }
+                for (int i = 0; i < countDigital; i++)
+                {
+                    digitalVal[i] = rndBoolean();
+                }
+                for (int i = 0; i < countEnums; i++)
+                {
+                    enumVal[i] = ran.Next(10);
+                }
+
             }
 
         }
@@ -64,6 +76,11 @@ namespace EvolutionAlgo
         {
 
 
+        }
+        private bool rndBoolean()
+        {
+            Random rand = new Random();
+            return rand.Next(0, 2) == 0;
         }
     }
 }
