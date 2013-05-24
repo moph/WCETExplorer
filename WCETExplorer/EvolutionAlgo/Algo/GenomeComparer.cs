34fdafb6fff818652be6d2dc20d9aa23c6f2e841
josuekiefer <josuekiefer@gmx.de>
2013-05-24 20:12:46 +0200

    class GenomeComparer added

---
+++ WCETExplorer/EvolutionAlgo/Algo/GenomeComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace EvolutionAlgo.Algo
+{
+    class GenomeComparer : IComparer
+    {
+        int IComparer.Compare(object a, object b)
+        {
+            Genom g1 = (Genom)a;
+            Genom g2 = (Genom)b;
+
+            if (g1.fittness > g2.fittness)
+                return 1;
+            if (g1.fittness < g2.fittness)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
