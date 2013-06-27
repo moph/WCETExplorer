a0b863b49d617583734473d2798ded04f88dca58
josuekiefer <josuekiefer@gmx.de>
2013-06-28 00:37:43 +0200

    Die Größe der Generation nach der Selektion ist falsch. Es wird _aSettings.populationSize (ist immer 10) verwendet richtig wäre aber die erhaltene ArrayList.Count.

--- WCETExplorer/EvolutionAlgo/Algo/EvolutionAlgo.cs
+++ WCETExplorer/EvolutionAlgo/Algo/EvolutionAlgo.cs
@@ -111,7 +111,7 @@ namespace EvolutionAlgo
                     myGeneration.mutate();
                     // Select Genes from Generation with selected Selection Strategy & Create new Generation but use existing genoms.
                     myGeneration = new Generation(_aSettings.strategy.select(generation2Array(myGeneration)), _aSettings.populationSize, _aSettings.mutationRate, _aSettings.crossoverCount, this);
-                    // Change bestGenom if necessary.
+                    // Change bestGenom if necessary. 
                     if (_bestGenom.fittness < myGeneration.getBestGenom().fittness) {
                         _bestGenom = myGeneration.getBestGenom();
                     }
